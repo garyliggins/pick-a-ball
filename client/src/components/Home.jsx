7eb3d821dd81255be97ef32248e2b4d890eab442
geovonnie vasquez <geovonnievasquez@gmail.com>
2021-03-04 23:20:07 -0600

    home comp done not tested on postman yet

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 // import nav component
 import NavbarComp from "./NavbarComp";
-import { Jumbotron, Container } from "react-bootstrap";
+import { Jumbotron, Container, Button } from "react-bootstrap";
+// axios to make calls to backend
+import axios from "axios";
 
 const Home = () => {
+
+  const [games,setAllGames] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/games")
+      .then((res) => setAllGames(res.data))
+      .catch((err) => console.log(err))
+  }, []);
+
+
   return (
     <div>
       <NavbarComp />
@@ -11,10 +23,22 @@ const Home = () => {
         <h1 className="homeHead">Upcoming Games</h1>
         <Jumbotron fluid className="homeJumbo">
           <Container>
-            <h1>No Upcoming Games</h1>
+            {
+              games.map((game,index) => (
+                <div key={index}>
+                  <h3>You Have a {game.sport} game on {game.date}</h3>
+                  <p>be there by {game.time} @ {game.location}</p>
+                  <Button variant="outline-primary">Delte Game</Button>{' '}
+                </div>
+              ))
+            };
+
+            {/* if theres no games show this in the jumbotron */}
+
+            {/* <h1>No Upcoming Games</h1>
             <p>
               add games to attend!
-            </p>
+            </p> */}
           </Container>
         </Jumbotron>
       </div>
