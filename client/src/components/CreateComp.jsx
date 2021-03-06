512afe7212c00bc17dbf091cae879653d0970e24
Paul Menendez <PaulGMenendez@gmail.com>
2021-03-06 11:51:50 -0700

    working on create

--- client/src/components/CreateComp.jsx
+++ client/src/components/CreateComp.jsx
@@ -1,21 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form, Row, Col, InputGroup, Button } from "react-bootstrap";
 import NavbarComp from "./NavbarComp";
 
 const CreateComp = () => {
+
+const [game, setGame] = useState("");
+const [sport, setSport] = useState("");
+const [city, setCity] = useState("");
+const [state, setState] = useState("");
+const [location, setLocation] = useState("");
+const [players, setPlayers] = useState("");
+const [time, setTime] = useState("");
+
+const addGame = (e) => {
+  e.preventDefault();
+  axios
+      .get("http://localhost:8000/api/games")
+      .then((res) => {
+          axios
+              .post("http://localhost:8000/api/games/", {
+                  sport: sport,
+                  city: city,
+                  state: state,
+                  location: location,
+                  players: players,
+                  time: time,
+              })
+              .then((res) => {
+                  if (res.data.error) {
+                      setErrors(res.data.error.errors);
+                      console.log(errors);
+                      console.log(res.data);
+                  }
+                  else {
+                      setGame(res.data);
+                      console.log(deviation);
+                      navigate(`/api/games/${res.data._id}`)
+                      console.log(res.data);
+                  }
+              })
+              .catch((err) => console.log(err));       
+      })
+  .catch(err => console.log(err));
+  console.log("addGame started");
+}
+
   return (
     <div>
       <NavbarComp />
       <h1 className="create-top">Create a Game</h1>
       <div className="contianer from form-comp-container">
-        <Form>
+        <Form onSubmit={addGame}>
             {/* sport */}
           <Form.Group as={Row}>
             <Form.Label column sm="2"> 
               Sport
             </Form.Label>
             <Col sm="10">
-            <Form.Control size="md" type="text" placeholder="Sport" />
+            <Form.Control size="md" type="text" placeholder="Sport" onChange={(e) => setSport(e.target.value)}/>
             </Col>
           </Form.Group>
           {/* City */}
@@ -24,7 +66,7 @@ const CreateComp = () => {
               City
             </Form.Label>
             <Col sm="10">
-            <Form.Control size="md" type="text" placeholder="City" />
+            <Form.Control size="md" type="text" placeholder="City" onChange={(e) => setCity(e.target.value)}/>
             </Col>
           </Form.Group>
           {/* State */}
