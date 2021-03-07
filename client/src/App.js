011f0922c371dfe61780d34b2023d7f40d06f74a
unknown <geovonnnievasquez@gmail.com>
2021-03-06 23:26:44 -0600

    times updated and date

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,16 @@ import "./styles/app.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 // componetns
 import Home from "./components/Home";
-import FindGameComp from "./components/FindGameComp";
 import CreateComp from "./components/CreateComp";
-
+import UpdateGame from "./components/UpdateGame";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Home path="/" />
-        <FindGameComp path="/findGame" />
         <CreateComp path="/createGame" />
+        <UpdateGame path="/updateGame" />
       </Router>
     </div>
   );
---
+++ client/src/components/UpdateGame.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NavbarComp from "./NavbarComp";
+
+const UpdateGame = () => {
+    return(
+        <NavbarComp />
+    )
+};
+
+export default UpdateGame;
